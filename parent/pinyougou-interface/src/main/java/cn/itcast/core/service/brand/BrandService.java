ae912c75be27347d2a011e54b4d2aeba04342a5a
HahAmypage <13632439133@139.com>
2019-03-16 00:19:53 +0800

    创建工程结构

---
+++ parent/pinyougou-interface/src/main/java/cn/itcast/core/service/brand/BrandService.java
@@ -0,0 +1,12 @@
+package cn.itcast.core.service.brand;
+
+import cn.itcast.core.pojo.good.Brand;
+
+import java.util.List;
+
+public interface BrandService {
+    /**
+     * 查询所有品牌
+     */
+    List<Brand> findAll();
+}
---
+++ parent/pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/brand/BrandServiceImpl.java
@@ -0,0 +1,23 @@
+package cn.itcast.core.service.brand;
+
+import cn.itcast.core.dao.good.BrandDao;
+import cn.itcast.core.pojo.good.Brand;
+import com.alibaba.dubbo.config.annotation.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class BrandServiceImpl implements BrandService {
+
+    @Resource
+    private BrandDao brandDao;
+
+    /**
+     * 查询所有品牌
+     */
+    @Override
+    public List<Brand> findAll() {
+        return brandDao.selectByExample(null);
+    }
+}
